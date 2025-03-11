c2fb3226de1a37f6d05ca5405f368c480515b94e
yasinmer39 <mericyasin691@gmail.com>
2025-03-11 12:47:56 +0000

    visual kamera eklendi

--- scripts/pseudo_thermal_camera.py
+++ scripts/pseudo_thermal_camera.py
@@ -2,14 +2,24 @@ from picamera2 import Picamera2
 import cv2
 import numpy as np
 
-# Initialize camera
-picam2 = Picamera2()
-video_config = picam2.create_video_configuration()
-picam2.configure(video_config)
-picam2.start()
+# Initialize NoIr camera
+noir_cam = Picamera2(0)
+video_config_0 = noir_cam.create_video_configuration()
+noir_cam.configure(video_config_0)
+noir_cam.start()
+
+# Initialize Visual camera
+visual_cam = Picamera2(1)
+video_config_1 = visual_cam.create_video_configuration()
+visual_cam.configure(video_config_1)
+visual_cam.start()
 
 while True:
-    frame = picam2.capture_array()
+    frame = noir_cam.capture_array()
+    frame1 = visual_cam.capture_array()
+    
+    #BGR RGB yap
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
     
     # Convert to grayscale (intensity-based)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -19,9 +29,11 @@ while True:
     
     # Show the result
     cv2.imshow("Pseudo Thermal Camera", thermal)
+    cv2.imshow("Visual CAM", frame1)
     
     if cv2.waitKey(1) == ord('q'):
         break
 
-picam2.close()
+noir_cam.close()
+visual_cam.close()
 cv2.destroyAllWindows()
